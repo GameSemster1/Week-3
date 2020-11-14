d64d010faa3224ed0b9e80c94f4f36c1b5a041ce
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-11-14 22:03:50 +0200

    init & 3_life

---
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+    public Image[] hearts; // Image of heart
+    private int life = 3;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == triggeringTag && enabled)
+        {
+            life--;
+            Destroy(hearts[life]);
+            if (life == 0)
+            {
+                Destroy(this.gameObject);
+                Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
